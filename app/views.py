65c7c2e5fae7fdf77f71a18c25d63b361f882ec4
clj <cljqqyx@qq.com>
2018-09-03 09:22:14 +0800

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+def login(request):
+    return render(request, 'login.html')
