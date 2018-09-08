46803914ef9b020dd1c33e03b86d14f9e94facc8
xiaopao <cljqqyx@qq.com>
2018-09-08 14:44:03 +0800

    new

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django_apscheduler.models import DjangoJob
 # Create your models here.
 class Exchange(models.Model):
     code=models.CharField('代号',max_length=30)
@@ -11,3 +11,18 @@ class Exchange(models.Model):
 class Symbol(models.Model):
     exchange=models.ForeignKey('Exchange',on_delete=models.CASCADE,null=True)
     name=models.CharField('对名称', max_length=30,default='')
+class Task(models.Model):
+    name=models.CharField('任务名称', max_length=100,unique=True)
+    minute=models.CharField('分', max_length=10,)
+    hour = models.CharField('时', max_length=10)
+    day = models.CharField('天', max_length=10)
+    exid =models.IntegerField('交易所ID')
+    symbol=models.CharField('交易对', max_length=20)
+    amount=models.DecimalField('金额',max_length=20)
+    sellpercent=models.DecimalField('卖出比率',max_length=10)
+    cost=models.DecimalField('成本，max_length=50')
+    ttime=models.TimeField('任务时间',auto_now=True)
+class Tasklog(models.Model):
+    task=models.ForeignKey('Task',on_delete=models.CASCADE)
+    tltime=models.TimeField('日志时间',auto_now_add=True)
+    content=models.TextField('日志内容',max_length=2000)
--- app/views.py
+++ app/views.py
@@ -5,17 +5,108 @@ from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
 from django.contrib import messages
-import json
-import ccxt
-import re
-
 from django.urls import reverse
 from django.shortcuts import redirect
 from django.http import HttpResponse
+import json
+import ccxt
+import re
+from decimal import Decimal
+
+e
 
 from .form import *
 from .models import *
 
+
+
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
+
+scheduler = BackgroundScheduler()
+scheduler.add_jobstore(DjangoJobStore(), "default")
+
+scheduler.start()
+
+
+def writetasklog(tid,content):
+    tasklog = Tasklog.objects.create(task_id=tid)
+    tasklog.content = content
+    tasklog.save()
+
+def timetoorder(exid,tid,symbol,amount,sellpercent):
+    task = Task.objects.get(pk=tid)
+    exchange = Exchange.objects.get(pk=exid)
+    ex = eval("ccxt." + exchange.code + "()")
+    ex.apiKey = exchange.apikey
+    ex.secret = exchange.secretkey
+    ex.options['createMarketBuyOrderRequiresPrice'] = False
+    try:
+        cost=task.cost
+        firstsymbol=symbol.split('_')[0]
+        quatity=Decimal(ex.fetchbalance()[firstsymbol]['free'])
+        orderbook = ex.fetch_order_book(symbol=symbol)
+        bid = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
+        ask = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
+        averageprice = Decimal((ask + bid) / 2)
+        if averageprice*quatity > cost * (1+(sellpercent/100)):
+            sellorderdata=exchange.create_market_sell_order(symbol=symbol, amount=quatity)
+            if sellorderdata['info']['status'] == 'ok':
+                content='定投收益已达到'+sellpercent+'%,成功卖出'
+                writetasklog(tid,content)
+    except:
+        content = '定投卖出异常'
+        writetasklog(tid, content)
+        pass
+
+    try:
+        buyorderdata=ex.create_market_buy_order(symbol=symbol, amount=amount)
+        if buyorderdata['info']['status'] == 'ok':
+            task.cost+=amount
+            content = '定投成功买入'+str(amount)+'金额的'+str(symbol.split('_')[1])
+            writetasklog(tid, content)
+    except:
+        content = '定投买入异常'
+        writetasklog(tid, content)
+        pass
+
+####
+def taskadd(request):
+    if request.method=='POST':
+        pass
+def taskload(request,tid):
+    task=Task.objects.get(pk=tid)
+    job=scheduler.get_job(job_id=str(tid))
+    if job:
+        scheduler.reschedule_job("cron", id=str(tid), day=task.day,hour=task.hour, minute=task.minute, second=0, kwargs={'exid': task.exid,'tid':tid,'symbol':task.symbol,'amount':task.amount,'sellpercent':task.sellpercent})
+        job.resume()
+        messages.add_message(request, messages.INFO, '任务' + task.name + '重载成功')
+    else:
+        scheduler.add_job(timetoorder, "cron", id=str(tid), day=task.day,hour=task.hour, minute=task.minute, second=0, kwargs={'exid': task.exid,'tid':tid,'symbol':task.symbol,'amount':task.amount,'sellpercent':task.sellpercent})
+        messages.add_message(request, messages.INFO, '任务' + task.name + '启动成功')
+    register_events(scheduler)
+
+    return redirect(reverse('task', args=[]))
+def taskpause(request,tid):
+    task = Task.objects.get(pk=tid)
+    job = scheduler.get_job(job_id=str(tid))
+    if job:
+        job.pause()
+        messages.add_message(request, messages.INFO, '任务' + task.name + '已暂停')
+    return redirect(reverse('task', args=[]))
+def taskdel(request,tid):
+    task = Task.objects.get(pk=tid)
+    job = scheduler.get_job(job_id=str(tid))
+    if job:
+        task.delete()
+        job.remove()
+    return redirect(reverse('task', args=[]))
+def task(request):
+    tasks=Task.objects.all()
+    jobs=DjangoJob.objects.all()
+
+    return render(request, 'task.html', {})
 
 # Create your views here.
 
@@ -67,27 +158,31 @@ def exchangeinfo(request,exid):
 
 def symbollist(request,exid):
     exchange = Exchange.objects.get(pk=exid)
-    usymbols=exchange.symbols.split()
-    symbols=[]
-    for symbol in usymbols:
-        symbol=re.sub('/','_',symbol)
-        symbols.append(symbol)
+    usymbols = exchange.symbols
     search = request.GET.get('search')
-    if search:
-        tmpsymbols = []
-        for symbol in symbols:
-            if search.upper() in symbol:
-                tmpsymbols.append(symbol)
-        symbols = tmpsymbols
-    else:
-        paginator = Paginator(symbols, 20)
-        page = request.GET.get('page')
-        try:
-            symbols = paginator.page(page)
-        except PageNotAnInteger:
-            symbols = paginator.page(1)
-        except EmptyPage:
-            symbols = paginator.page(paginator.num_pages)
+    symbols = []
+    if usymbols:
+        usymbols=usymbols.split()
+
+        for symbol in usymbols:
+            symbol=re.sub('/','_',symbol)
+            symbols.append(symbol)
+
+        if search:
+            tmpsymbols = []
+            for symbol in symbols:
+                if search.upper() in symbol:
+                    tmpsymbols.append(symbol)
+            symbols = tmpsymbols
+        else:
+            paginator = Paginator(symbols, 20)
+            page = request.GET.get('page')
+            try:
+                symbols = paginator.page(page)
+            except PageNotAnInteger:
+                symbols = paginator.page(1)
+            except EmptyPage:
+                symbols = paginator.page(paginator.num_pages)
 
     return render(request, 'symbollist.html', {'exchange':exchange,'symbols': symbols,'search':search})
 
@@ -96,15 +191,21 @@ def symbolupdate(request,exid):
     exchange = Exchange.objects.get(pk=exid)
     ex=eval("ccxt."+exchange.code+"()")
     ex.load_markets()
-    exchange.symbols=ex.symbols
+    symbols=' '.join(ex.symbols)
+    exchange.symbols=symbols
     exchange.save()
     return redirect(reverse('symbollist',args=[exid,]))
 
 def symboladd(request,exid,symbol):
     exchange = Exchange.objects.get(pk=exid)
-    symbol=Symbol.objects.get_or_create(name=symbol,exchange_id=exid)[0]
-    symbol.save()
-    messages.add_message(request, messages.INFO, exchange.name+symbol.name+'交易对添加成功')
+    esymbol=Symbol.objects.filter(name=symbol,exchange_id=exid)
+
+    if esymbol.exists():
+        messages.add_message(request, messages.INFO, exchange.name + str(symbol) + '交易对已存在')
+    else:
+        esymbol=Symbol.objects.create(name=symbol,exchange_id=exid)
+        esymbol.save()
+        messages.add_message(request, messages.INFO, exchange.name+str(symbol)+'交易对添加成功')
 
 
     return redirect(reverse('symbollist', args=[exid, ]))
